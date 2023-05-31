44047b7a6a910505b6b0e49ae7e1ad8fa10f0b78
sohelranagtc <sohelranagtc@gmail.com>
2023-05-31 17:28:40 +0600

    added JWT

--- src/Hooks/useCart.jsx
+++ src/Hooks/useCart.jsx
@@ -4,11 +4,12 @@ import { useQuery } from "@tanstack/react-query"
 
 const useCart = email => {
     const { user } = useContext(AuthContext)
+    const token = localStorage.getItem('access-token')
 
     const { refetch, data : cart = [] } = useQuery({
         queryKey: ['carts', user?.email],
         queryFn: async () =>{
-            const res = await fetch(`http://localhost:5000/carts?email=${user?.email}`)
+            const res = await fetch(`http://localhost:5000/carts?email=${user?.email}`, {headers : {authorization : `bearer ${token}`}})
             return res.json()
         }
     })
